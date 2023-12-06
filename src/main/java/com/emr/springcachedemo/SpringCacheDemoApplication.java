8d063afaab309dffa9a55adb7e09fba07542d127
emrullahozkilinc <emr.ozk.71@gmail.com>
2023-12-06 10:54:20 +0300

    getting-started redis doc

--- src/main/java/com/emr/springcachedemo/SpringCacheDemoApplication.java
+++ src/main/java/com/emr/springcachedemo/SpringCacheDemoApplication.java
@@ -2,13 +2,19 @@ package com.emr.springcachedemo;
 
 import com.emr.springcachedemo.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @SpringBootApplication
 @RequiredArgsConstructor
 public class SpringCacheDemoApplication implements CommandLineRunner {
@@ -19,5 +25,25 @@ public class SpringCacheDemoApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {}
+    public void run(String... args) throws Exception {
+
+        RedisStandaloneConfiguration redisSC = new RedisStandaloneConfiguration();
+        redisSC.setPassword("cache-demo");
+        redisSC.setPort(8640);
+
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisSC);
+        connectionFactory.afterPropertiesSet();
+
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setDefaultSerializer(StringRedisSerializer.UTF_8);
+        template.afterPropertiesSet();
+
+        template.opsForValue().set("foo", "bar");
+
+        log.info("Value at foo:" + template.opsForValue().get("foo"));
+
+        connectionFactory.destroy();
+
+    }
 }
--- src/main/java/com/emr/springcachedemo/configuration/CachingConfiguration.java
+++ src/main/java/com/emr/springcachedemo/configuration/CachingConfiguration.java
@@ -1,17 +1,68 @@
-package com.emr.springcachedemo.configuration;
-
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableCaching //ConcurrentMapCacheManager'ı aktive eder ve ayrıca bunun için bir bean açmamıza gerek kalmaz.
-public class CachingConfiguration {
-
+//package com.emr.springcachedemo.configuration;
+//
+//import com.fasterxml.jackson.annotation.JsonAutoDetect;
+//import com.fasterxml.jackson.annotation.PropertyAccessor;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.springframework.cache.CacheManager;
+//import org.springframework.cache.annotation.CachingConfigurerSupport;
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.cache.interceptor.KeyGenerator;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.cache.RedisCacheManager;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+//
+//import java.lang.reflect.Method;
+//
+//@Configuration
+//@EnableCaching //ConcurrentMapCacheManager'ı aktive eder ve ayrıca bunun için bir bean açmamıza gerek kalmaz.
+//public class CachingConfiguration extends CachingConfigurerSupport {
+//
+//    private static final String KEY_SEPERATOR = "#";
+//
 //    @Bean
-//    public CacheManager cacheManager() {
-//        return new ConcurrentMapCacheManager("addresses");
+//    @Override
+//    public KeyGenerator keyGenerator() {
+//        return (target, method, params) -> {
+//
+//            StringBuilder sb = new StringBuilder();
+//            sb.append(target.getClass().getSimpleName());
+//            sb.append(KEY_SEPERATOR);
+//            sb.append(method.getName());
+//            sb.append(KEY_SEPERATOR);
+//            for(Object param : params){
+//                sb.append(param.toString());
+//                sb.append(KEY_SEPERATOR);
+//            }
+//
+//            String str = sb.toString();
+//
+//            return str.substring(0, str.length() - 1);
+//        };
 //    }
-}
+//
+//    @Bean
+//    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+//        RedisCacheManager manager = new RedisCacheManager(redisTemplate);
+//        manager.setExpires(7200);
+//        return manager;
+//    }
+//
+//    @Bean
+//    public StringRedisTemplate redisTemplate(RedisConnectionFactory factory) {
+//        final StringRedisTemplate template = new StringRedisTemplate(factory);
+//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+//        ObjectMapper om = new ObjectMapper();
+//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//        jackson2JsonRedisSerializer.setObjectMapper(om);
+//        template.setValueSerializer(jackson2JsonRedisSerializer);
+//        template.afterPropertiesSet();
+//
+//        return template;
+//    }
+//
+//}
